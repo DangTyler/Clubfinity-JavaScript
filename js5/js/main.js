03d36a1e63087d39050adbd44dee264367836ca4
DangTyler <151698295+DangTyler@users.noreply.github.com>
2025-03-17 04:11:23 -0400

    Chapter 5

---
+++ js5/js/main.js
@@ -0,0 +1,28 @@
+// Math Methods and Properties
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI)); // 3
+console.log(Math.round(3.4)); // 3
+console.log(Math.round(3.5)); // 4
+console.log(Math.ceil(3.4)); // 4
+console.log(Math.floor(3.4)); // 3
+console.log(Math.pow(5, 2)) // 5^2
+console.log(Math.min(2, 0.5, 9));
+console.log(Math.max(2, 0.5, 9))
+console.log(Math.max(2, 9.2, 9))
+
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+// Use floor to return a range
+// Slim chance that random returns 0, and math.ceil will be 0
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
